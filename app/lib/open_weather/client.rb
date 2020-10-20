1150c2d627dfd604d6b1b088373615630efaa5e6
Michał Kajder <michal.kajder@themasters.io>
2020-10-20 23:22:48 +0200

    Add client

---
+++ app/lib/open_weather/client.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+
+module OpenWeather
+  class Client
+    HOST = 'https://api.openweathermap.org/data/2.5'.freeze
+    def initialize(api_key: Rails.configuration.open_weather_token)
+      @api_key = api_key
+    end
+
+    def forecast(city, options = {})
+      url = "#{HOST}/forecast"
+      fetch(url, options.merge(q: city))
+    end
+
+    def fetch(url, options = {})
+      uri = URI(url)
+      uri.query = URI.encode_www_form(options.merge(appid: api_key))
+      response = Net::HTTP.get(uri)
+      OpenWeather::Response.new(response)
+    end
+
+    private
+
+    attr_reader :api_key
+  end
+end
---
+++ app/lib/open_weather/response.rb
@@ -0,0 +1,16 @@
+module OpenWeather
+  class Response
+    attr_reader :status, :message, :list
+
+    def initialize(response)
+      body = JSON.parse(response)
+      @status = body['cod']
+      @message = body['message']
+      @list = body['list']
+    end
+
+    def success?
+      status == '200'
+    end
+  end
+end
